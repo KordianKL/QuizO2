139f87e5906167ae62b714bf63f606af5225a067
Kordian Ledzion <kkordikk@gmail.com>
2018-04-12 22:35:58 +0200

    Add Answer model

---
+++ QuizO2/QuizO2/Model/Answer.swift
@@ -0,0 +1,17 @@
+//
+//  Answer.swift
+//  QuizO2
+//
+//  Created by Kordian Ledzion on 12/04/2018.
+//  Copyright © 2018 KordianLedzion. All rights reserved.
+//
+
+import Foundation
+
+struct Answer {
+    
+    let imageUrl: String?
+    let text: String?
+    let isCorrect: Bool?
+    
+}
